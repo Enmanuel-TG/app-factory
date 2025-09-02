32d051bef21550c8c5072e10003a09e97a37dd0a
Enmanuel-TG <enmanueltg7@gmail.com>
2025-09-02 00:06:47 -0400

    add auth secret

--- auth.ts
+++ auth.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github"
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -8,4 +8,5 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientSecret: process.env.AUTH_GITHUB_SECRET,
     }),
   ],
+  secret: process.env.AUTH_SECRET,
 });
